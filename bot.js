296df208bad9648cb761527f7f6fe6b44def3162
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-23 11:16:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,16 +11,7 @@ client.on('message', message => {
       }
 });
 
-client.on('message', (message) => {
-    if (message.content.startsWith('^^kick')) {
-        var member= message.mentions.members.first();
-        member.kick().then((member) => {
-            message.channel.send(member.displayName + ' تم طرد هذا الشخص من السيرفر');
-        }).catch(() => {
-            message.channel.send(":x:");
-        });
-    }
-}); 
+
 
 client.on('message', msg => {
      if(msg.content === 'kd') {
