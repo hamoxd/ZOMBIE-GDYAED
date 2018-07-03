8108a23a99c4618aecc2ad666126f9a4d07fe4bb
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-07-04 00:10:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,6 +79,16 @@ client.on('message', msg => {
     msg.reply('**ۆعـلُـيَـگـم آلُـسـلُآم ۆرحٍـمةّ آلُلُہ ۆبْرگآتٌہ ، منْۆر. :rose: **');
   }
 });
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`HD_4EVER`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 
 // THIS  MUST  BE  THIS  WAY
